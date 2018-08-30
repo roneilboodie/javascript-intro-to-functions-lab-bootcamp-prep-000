2c8e6f79c1a84fccf0a18ff2e8c543679306b3c6
boodieroneil@gmail.com <boodieroneil@gmail.com>
2018-08-30 00:18:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,18 @@ function logWhisper(String) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
+<<<<<<< HEAD
   return "I can't hear you!" }
     if (string === string.toUpperCase()) {
       return "YES INDEED!" }
       if (string === "I love you, Grandma.") {
         return "I love you, too."  }
+=======
+  return "I can't hear you!" 
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+      if (string === "I love you, Grandma.") {
+        return "I love you, too"
+  }
+>>>>>>> a2d11ad8416d0b36c25036892932c3e33306b3d9
 }
